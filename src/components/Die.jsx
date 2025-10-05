b1a7ade37d8a3482efb88b6801a0bfa60fbfd194
DarkoHristovski <hristovski15@gmail.com>
2025-10-05 23:18:40 +0200

    add new functionalities

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 const Die = (props) =>{
+
+const styles = {
+backgroundColor:props.die.isHeld ? '#59E391':'#fff'
+}
+
+
+
+const onClickHandler = (id) =>{
+    props.hold(id);
+
+    console.log(props.die.isHeld)
+}
+
+
     return(
-  
-        <button className="die">{props.value}</button>
+ 
+        <button style={styles}  onClick={()=>onClickHandler(props.die.id)} className="die">{props.die.value}</button>
       
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,26 +1,74 @@
-import { use } from "react"
+
 import Die from "./Die"
 import { useState } from "react"
+import {nanoid} from "nanoid";
 
-
+function generateAllnewDice(){
+  return new Array(10).fill(0)
+  .map(()=> ({
+     value: Math.ceil(Math.random() * 6),
+     isHeld:false,
+     id:nanoid(),
+    }));
+}
 
 const Main = () =>{
+
+  const [randomNumber, setRandomNumber]=useState(generateAllnewDice);
+
+
+   
+
+
+    const gameWon = randomNumber.every(x=>x.isHeld) &&
+                    randomNumber.every(x=>x.value=== randomNumber[0].value);
     
+
+    const hold = (id) =>{
+      setRandomNumber(state => {
+        return state.map(x=>{
+          return x.id === id ?
+          {...x, isHeld: !x.isHeld} : x;
+        })
+      })
+
+    }
+
+    
+    const result = randomNumber.map((x)=><Die 
+    key={x.id} 
+    hold={hold} 
+    die={x}/>);
+
+    const handlerDies = () =>{
+      if(!gameWon){
+        setRandomNumber(oldDice => oldDice.map(x=>
+          x.isHeld ? 
+          x:
+          {...x, value:Math.ceil(Math.random() * 6)}
+        ));
+      }else{
+        setRandomNumber(generateAllnewDice());
+      }
+      
+    }
+   
+
 
     return (
         <div className="main">
             <div className="main-inner-section">
+              <div className="text-section">
+                <h2>Tenzies</h2>
+             <p> Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+              </div>
             <div className="dies-container">   
-                <Die value={1}/>
-                <Die value={2}/>
-                <Die value={3}/>
-                <Die value={4}/>
-                <Die value={5}/>
-                <Die value={6}/>
-                <Die value={1}/>
-                <Die value={2}/>
-                <Die value={3}/>
-                <Die value={4}/>
+              {result}
+            </div>
+            <div className="btn-section">
+            <button onClick={handlerDies}>
+            { gameWon ? 'Play Again' : 'Roll'}
+              </button>
             </div>
         </div>
         </div>   
